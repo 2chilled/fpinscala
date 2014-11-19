499bcd6ffe263793de0a16c08693ccd5aae23f46
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2014-11-19 19:33:18 +0100

    filter using flatMap

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -94,4 +94,6 @@ object List {
   def doubleToString(l: List[Double]): List[String] = map(l)(_.toString)
 
   def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = join(map(as)(f))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(a => if(f(a)) List(a) else Nil)
 }
