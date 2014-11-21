55f7d406cbe627208ba6c77e36c96b9339185848
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2014-11-21 13:21:28 +0100

    small refactoring

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,13 +1,15 @@
 package fpinscala.datastructures
 
 sealed trait Tree[+A]
+
 case class Leaf[A](value: A) extends Tree[A]
+
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
 
-  def fold[A,B](t:Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
     case Leaf(a) => f(a)
     case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
   }
@@ -18,7 +20,7 @@ object Tree {
 
   def depth[A](t: Tree[A]): Int = fold(t)(i => 1)((b1, b2) => (1 + b1).max(1 + b2))
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
-    fold(t)((a => Leaf(f(a)):Tree[B]))((b1,b2) => Branch(b1,b2))
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)((a => Leaf(f(a)): Tree[B]))(Branch(_, _))
 
 }
